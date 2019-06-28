90254624feda98f254a417919154190461a9e419
NicVanZuylen <nicvanzuylen@gmail.com>
2019-06-28 17:08:16 +1000

    Fixed:
    -Bumpiness when the player walks down a slope.

--- Overscoped - BossFight/Assets/Code/Scripts/PlayerController.cs
+++ Overscoped - BossFight/Assets/Code/Scripts/PlayerController.cs
@@ -36,6 +36,17 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 m_velocity;
 
+    [SerializeField]
+    private bool m_onGround;
+
+    // Surface variables.
+    private BoxCollider m_groundDetectionBox;
+    private bool m_boxDetectedGround;
+
+    private Vector3 m_surfaceRight;
+    private Vector3 m_surfaceUp;
+    private Vector3 m_surfaceForward;
+
     Transform m_cameraTransform;
     private float m_lookEulerX;
     private float m_lookEulerY;
@@ -50,6 +61,9 @@ public class PlayerController : MonoBehaviour
         m_controller = GetComponent<CharacterController>();
         m_collider = GetComponent<CapsuleCollider>();
         m_cameraTransform = GetComponentInChildren<Camera>().transform;
+        m_groundDetectionBox = GetComponent<BoxCollider>();
+
+        Physics.IgnoreCollision(m_groundDetectionBox, m_controller);
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -57,36 +71,41 @@ public class PlayerController : MonoBehaviour
 
     public void Jump()
     {
-        m_velocity += m_jumpForce * transform.up;
+        m_velocity += m_jumpForce * m_surfaceUp;
     }
 
     public Vector3 WASDAcceleration(float acceleration)
     {
+        Debug.Log(m_surfaceRight);
+
+        Debug.DrawLine(transform.position, transform.position + (m_surfaceRight * 3.0f), Color.red);
+        Debug.DrawLine(transform.position, transform.position + (m_surfaceUp * 3.0f), Color.green);
+        Debug.DrawLine(transform.position, transform.position + (m_surfaceForward * 3.0f), Color.blue);
+
         Vector3 netForce = Vector3.zero;
         bool bMoved = false;
 
+        // On the ground, move relative to the camera and the surface.
         if (Input.GetKey(KeyCode.W))
         {
             bMoved = true;
-            netForce += m_cameraTransform.forward * acceleration * Time.deltaTime;
+            netForce += m_surfaceForward * acceleration * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
             bMoved = true;
-            netForce -= m_cameraTransform.right * acceleration * Time.deltaTime;
+            netForce -= m_surfaceRight * acceleration * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
             bMoved = true;
-            netForce -= m_cameraTransform.forward * acceleration * Time.deltaTime;
+            netForce -= m_surfaceForward * acceleration * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
             bMoved = true;
-            netForce += m_cameraTransform.right * acceleration * Time.deltaTime;
+            netForce += m_surfaceRight * acceleration * Time.deltaTime;
         }
-
-        netForce.y = 0.0f;
 
         // Cancel diagonal movement speed boost.
         if (bMoved)
@@ -102,6 +121,7 @@ public class PlayerController : MonoBehaviour
 
     public Vector3 Drag(float drag)
     {
+        /*
         Vector3 velNor;
 
         if (m_velocity.magnitude > 1.0f)
@@ -109,9 +129,15 @@ public class PlayerController : MonoBehaviour
         else // Special case where speed is less than one. Normalizing the velocity here would cause an increase in speed.
             velNor = m_velocity;
 
-        velNor.y = 0.0f;
-
         return  -velNor * drag * Time.deltaTime;
+        */
+
+        float forwardComponent = Vector3.Dot(m_velocity, m_surfaceForward);
+        float lateralComponent = Vector3.Dot(m_velocity, m_surfaceRight);
+
+        Vector3 dragVector = (forwardComponent * m_surfaceForward) + (lateralComponent * m_surfaceRight);
+
+        return -dragVector.normalized * drag * Time.deltaTime;
     }
 
     public Vector3 GetVelocity()
@@ -173,6 +199,23 @@ public class PlayerController : MonoBehaviour
         return dir;
     }
 
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Collider collider = hit.collider;
+
+        if(collider != null && hit.point.y < transform.position.y) // Object must be the surface the player is standing on.
+        {
+            // Get surface-relative movement vectors.
+            m_surfaceUp = hit.normal;
+            m_surfaceForward = Vector3.Cross(m_cameraTransform.right, m_surfaceUp);
+            m_surfaceRight = Vector3.Cross(m_surfaceUp, m_surfaceForward);
+
+            //m_groundDetectionBox.transform.rotation = hit.collider.transform.rotation;
+
+            m_onGround = true;
+        }
+    }
+
     void Update()
     {
         // ------------------------------------------------------------------------------------------------------
@@ -204,18 +247,18 @@ public class PlayerController : MonoBehaviour
 
         // ------------------------------------------------------------------------------------------------------
         // Jumping
-        bool onGround = m_controller.isGrounded;
+        m_onGround = m_controller.isGrounded;// || m_boxDetectedGround;
 
-        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        if (Input.GetKeyDown(KeyCode.Space) && m_onGround)
         {
             Jump();
-            onGround = false;
+            m_onGround = false;
         }
 
         // ------------------------------------------------------------------------------------------------------
         // Movement
 
-        if(onGround)
+        if(m_onGround)
         {
             Vector3 outAcceleration = WASDAcceleration(m_groundAcceleration);
             m_netForce += outAcceleration;
@@ -224,11 +267,27 @@ public class PlayerController : MonoBehaviour
             {
                 m_netForce += Drag(m_groundDrag);
             }
+
+            /*
+            // Snap to surface.
+            Ray downRay = new Ray(transform.position, -m_surfaceUp);
+            RaycastHit rayHit = new RaycastHit();
+            Debug.DrawLine(transform.position, transform.position - (m_surfaceUp * 3.0f), Color.cyan);
+            if(Physics.Raycast(downRay, out rayHit, 5.0f))
+            {
+                Vector3 newPos = rayHit.point + (m_surfaceUp * (m_controller.height * 0.5f));
+                m_controller.Move(newPos - transform.position);
+            }
+            */
         }
         else
         {
             Vector3 outAcceleration = WASDAcceleration(m_airAcceleration);
             m_netForce += outAcceleration;
+
+            m_surfaceRight = m_cameraTransform.right;
+            m_surfaceUp = transform.up;
+            m_surfaceForward = m_cameraTransform.forward;
 
             m_netForce += Drag(m_airDrag);
         }
@@ -243,7 +302,7 @@ public class PlayerController : MonoBehaviour
         m_velocity += m_netForce;
 
         // Clamp velocity.
-        if (onGround && m_velocity.sqrMagnitude > m_maxSpeed * m_maxSpeed)
+        if (m_onGround && m_velocity.sqrMagnitude > m_maxSpeed * m_maxSpeed)
             m_velocity = m_velocity.normalized * m_maxSpeed;
         else if (m_velocity.sqrMagnitude > m_terminalVelocity * m_terminalVelocity)
             m_velocity = m_velocity.normalized * m_terminalVelocity;
@@ -253,4 +312,9 @@ public class PlayerController : MonoBehaviour
         // Set velocity to controller velocity output to apply any changes made by the controller physics.
         m_velocity = m_controller.velocity;
     }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    m_boxDetectedGround = other.transform.position.y < transform.position.y;
+    //}
 }

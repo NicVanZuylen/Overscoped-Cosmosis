cd61b5aa659bcc5baed209755d9fce8b1036a34c
NicVanZuylen <nicvanzuylen@gmail.com>
2019-09-26 15:03:59 +1000

    Added:
    -Volumetric beam effect for player beam.

--- Overscoped - BossFight_2019/Assets/Code/Scripts/PlayerBeam.cs
+++ Overscoped - BossFight_2019/Assets/Code/Scripts/PlayerBeam.cs
@@ -20,17 +20,16 @@ public class PlayerBeam : MonoBehaviour
     [SerializeField]
     private float m_fMinBeamCharge = 15.0f;
 
-    [Tooltip("Beam line renderer reference.")]
-    [SerializeField]
-    private LineRenderer m_beamLine = null;
-
     [Tooltip("Start point of the beam effect.")]
     [SerializeField]
     private Transform m_beamOrigin = null;
 
-    [Tooltip("Maximum range of the beam.")]
+    [Tooltip("Maximum range of the beam in metres.")]
     [SerializeField]
-    private float m_fBeamRange = 500.0f;
+    private int m_nBeamLength = 512;
+
+    [SerializeField]
+    private GameObject[] m_beamParticleObjects = null;
 
     private PlayerController m_controller;
     private CameraEffects m_camEffects;
@@ -41,29 +40,99 @@ public class PlayerBeam : MonoBehaviour
     private int m_nDissolveBracerIndex;
     private bool m_bCanCast;
     private bool m_bBeamUnlocked;
+    private bool m_bBeamEnabled;
+
+    // Volumetric beam effect.
+    private ParticleSystem[] m_beamParticles;
+    private ParticleSystemRenderer[] m_beamParticleRenderers;
+    private ParticleSystem.Particle[] m_particles;
+    private float m_fMeshLength;
 
     void Awake()
     {
         // Component retreival.
         m_controller = GetComponent<PlayerController>();
         m_camEffects = GetComponentInChildren<CameraEffects>();
-        m_beamLine.enabled = false;
 
         m_bBeamUnlocked = true;
 
         m_fBeamCharge = m_fMaxBeamCharge;
 
+        m_fMaxBeamCharge = 100000.0f;
+        m_fBeamCharge = 100000.0f;
+
+        m_beamParticles = new ParticleSystem[m_beamParticleObjects.Length];
+        m_beamParticleRenderers = new ParticleSystemRenderer[m_beamParticleObjects.Length];
+
+        for(int i = 0; i < m_beamParticleObjects.Length; ++i)
+        {
+            m_beamParticles[i] = m_beamParticleObjects[i].GetComponent<ParticleSystem>();
+            m_beamParticleRenderers[i] = m_beamParticleObjects[i].GetComponent<ParticleSystemRenderer>();
+        }
+
+        m_particles = new ParticleSystem.Particle[m_nBeamLength / 2];
+
+        m_fMeshLength = 2.0f;
+    }
+
+    /*
+    Description: Update volumetric beam particle systems.
+    Param:
+        ParticleSystem[] particleSystems: The particlesystems to update.
+        ParticleSystemRenderer[] renderers: The particle renderers to update material properties on.
+        ParticleSystem.Particle[] particles: Pre-allocated array of particles to update.
+        Vector3 v3Origin: Origin point of the beam.
+        float fLength: The length of the beam.
+        bool bHit: Whether or not the beam hit a surface.
+    */
+    private void UpdateParticlePositions(ParticleSystem[] particleSystems, ParticleSystemRenderer[] renderers, ParticleSystem.Particle[] particles, Vector3 v3Origin, float fLength, bool bHit)
+    {
+        int nParticleAmount = 0;
+
+        // Loop through all particles/segments and set their positions.
+        for(int i = 0; i < particles.Length; ++i)
+        {
+            float fOffset = i * m_fMeshLength;
+
+            // Set new offset.
+            particles[i].position = new Vector3(0.0f, 0.0f, fOffset);
+            particles[i].startSize = 1.0f;
+            particles[i].startColor = Color.white;
+            ++nParticleAmount;
+
+            // Stop once length is exceeded.
+            if (bHit && fOffset + m_fMeshLength >= fLength)
+            {
+                break;
+            }
+        }
+
+        for(int i = 0; i < m_beamParticles.Length; ++i)
+        {
+            // Set particle data.
+            particleSystems[i].SetParticles(m_particles, nParticleAmount);
+
+            // Set origin and length in shader.
+            renderers[i].material.SetVector("_LineOrigin", v3Origin);
+            renderers[i].material.SetFloat("_LineLength", fLength);
+        }
     }
 
     void LateUpdate()
     {
         if (m_bBeamUnlocked)
         {
-            m_bCanCast = m_beamLine.enabled || (Input.GetMouseButtonDown(1) && m_fBeamCharge >= m_fMinBeamCharge);
+            m_bCanCast = m_bBeamEnabled || (Input.GetMouseButtonDown(1) && m_fBeamCharge >= m_fMinBeamCharge);
 
-            m_beamLine.enabled = Input.GetMouseButton(1) && m_bCanCast && m_fBeamCharge >= 0.0f;
+            m_bBeamEnabled = Input.GetMouseButton(1) && m_bCanCast && m_fBeamCharge >= 0.0f;
 
-            if (m_beamLine.enabled)
+            // Enable/Disable effects.
+            for (int i = 0; i < m_beamParticles.Length; ++i)
+            {
+                m_beamParticleRenderers[i].enabled = m_bBeamEnabled;
+            }
+
+            if (m_bBeamEnabled)
             {
                 // Use the player controller's look rotation, to avoid offsets from camera shake.
                 m_beamOrigin.rotation = m_controller.LookRotation();
@@ -73,11 +142,14 @@ public class PlayerBeam : MonoBehaviour
 
                 const int nRaymask = ~(1 << 2); // Layer bitmask includes every layer but the ignore raycast layer.
                 Ray sphereRay = new Ray(m_beamOrigin.position, m_beamOrigin.forward);
-                bool bRayHit = Physics.Raycast(sphereRay, out m_sphereCastHit, m_fBeamRange, nRaymask, QueryTriggerInteraction.Ignore);
+                bool bRayHit = Physics.Raycast(sphereRay, out m_sphereCastHit, (float)m_nBeamLength, nRaymask, QueryTriggerInteraction.Ignore);
 
                 // Find end point of the line.
                 if (bRayHit)
                 {
+                    // Update volumetric beam particle positions.
+                    UpdateParticlePositions(m_beamParticles, m_beamParticleRenderers, m_particles, transform.position, m_sphereCastHit.distance, true);
+
                     float fHitProj = Vector3.Dot(m_sphereCastHit.point, m_beamOrigin.forward);
                     float fOriginProj = Vector3.Dot(m_beamOrigin.position, m_beamOrigin.forward);
 
@@ -87,12 +159,16 @@ public class PlayerBeam : MonoBehaviour
                 }
                 else
                 {
-                    m_v3BeamDestination = m_beamOrigin.position + (m_beamOrigin.forward * m_fBeamRange);
+                    // Update volumetric beam particle positions.
+                    UpdateParticlePositions(m_beamParticles, m_beamParticleRenderers, m_particles, transform.position, m_nBeamLength, false);
+
+                    m_v3BeamDestination = m_beamOrigin.position + (m_beamOrigin.forward * m_nBeamLength);
                 }
 
                 // Set linerenderer points.
-                m_beamLine.SetPosition(0, m_beamOrigin.position);
-                m_beamLine.SetPosition(1, m_v3BeamDestination);
+                //m_beamLine.SetPosition(0, m_beamOrigin.position);
+                //m_beamLine.SetPosition(1, m_v3BeamDestination);
+
 
                 // Reduce beam charge.
                 m_fBeamCharge -= m_fChargeLossRate * Time.deltaTime;
@@ -111,7 +187,7 @@ public class PlayerBeam : MonoBehaviour
     public void UnlockBeam(bool bUnlock)
     {
         m_bBeamUnlocked = bUnlock;
-        m_beamLine.enabled = false;
+        m_bBeamEnabled = false;
         enabled = bUnlock;
     }
 
@@ -128,7 +204,7 @@ public class PlayerBeam : MonoBehaviour
     */
     public bool BeamEnabled()
     {
-        return m_beamLine.enabled;
+        return m_bBeamEnabled;
     }
 
     /*
